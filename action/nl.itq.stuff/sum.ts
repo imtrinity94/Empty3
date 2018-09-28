5934b1d9f35efa5e93cb9f0cdec1fca125dd7a79
Flores Eken <flores@eken.nl>
2018-09-28 15:47:07 +0200

    A change

--- action/nl.itq.stuff/sum.ts
+++ action/nl.itq.stuff/sum.ts
@@ -1,7 +1,10 @@
 /// <reference path="./../../includes.d.ts"/>
 
 namespace nl.itq.stuff {
-    export function sum(a?: number, b?: number): number {
-        return a + b;
+    export function sum(a?: number, b?: VC.VirtualMachine): number {
+
+        var mult = nl.itq.stuff.multiply(a, 8);
+
+        return a + b[0] + mult;
     }
 }
