ac6b502421eb905ac4ddd800b073f8aa782eb1bc
Flores Eken <flores@eken.nl>
2019-02-24 10:30:30 +0100

    testing

--- action/nl.itq.stuff/multiply.ts
+++ action/nl.itq.stuff/multiply.ts
@@ -1,8 +1,7 @@
 /// <reference path="./../../includes.d.ts"/>
 
 namespace nl.itq.stuff {
-export function multiply(a?: number, b?: number): number {return a * b;
-
-
-}
+    export function multiply(a?: number, b?: number): number {
+        return a * b + a;
+    }
 }
